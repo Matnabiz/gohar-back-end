1a02fd66433f61372e041eed0f6761df863d01de
Matin Nabizadeh <matnabiz@yahoo.com>
2025-09-03 14:27:45 +0330

    fix: Some paths were incorrect.

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Models\Order;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
