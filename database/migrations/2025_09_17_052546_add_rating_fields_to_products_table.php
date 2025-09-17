e6bde4fc3de6334b5fc2b103d30cc660ef9d4dcc
Matin Nabizadeh <matnabiz@yahoo.com>
2025-09-17 08:56:21 +0330

    fix: Added migration for product rating.

--- database/migrations/2025_09_17_052546_add_rating_fields_to_products_table.php
+++ database/migrations/2025_09_17_052546_add_rating_fields_to_products_table.php
@@ -9,20 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->unsignedInteger('rating_count')->default(0);
+            $table->decimal('rating_avg', 3, 1)->nullable()->after('price'); // e.g. 4.3
+        });
+    }
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['rating_count','rating_avg']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('products', function (Blueprint $table) {
-            //
-        });
-    }
 };
