761dde89ad03b21a66ef2029646e88ba158c71bf
Matin Nabizadeh <matnabiz@yahoo.com>
2025-09-20 10:36:42 +0330

    fix: Secret Path.

--- app/Http/Controllers/Admin/MaintenanceController.php
+++ app/Http/Controllers/Admin/MaintenanceController.php
@@ -31,7 +31,7 @@ class MaintenanceController extends Controller
         $ip = $request->ip();
 
         if ($action === 'down') {
-            Artisan::call('down');
+            Artisan::call('down', ['--secret' => 'my-secret-path']);
         } else {
             Artisan::call('up');
         }
