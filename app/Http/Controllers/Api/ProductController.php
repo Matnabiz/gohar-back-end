3bd4c79a1a804c4e44617d5fc2a954b844f7bb4c
Matin Nabizadeh <matnabiz@yahoo.com>
2025-09-21 13:09:46 +0330

    feat: Product endpoint now returns dimensions as well.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -226,6 +226,7 @@ class ProductController extends Controller
             'main_image' => $product->main_image ? asset('storage/' . ltrim($product->main_image, '/')) : null,
             'images' => $imagesUrls,
             'material' => $product->material,
+            'dimensions' => $product->dimmensions,
             'color' => $product->color,
             'active' => $product->active,
             'stock' => $product->stock,
