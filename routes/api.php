f45cf8bb230089e58b6f4af630ace596d997d1e8
Matin Nabizadeh <matnabiz@yahoo.com>
2025-10-04 15:06:42 +0330

    feat: Modified the api URLs for the admin order filter.

--- routes/api.php
+++ routes/api.php
@@ -136,6 +136,7 @@ Route::post('orders', [OrderController::class, 'store']);
 Route::get('orders', [OrderController::class, 'index']);
 Route::get('orders/{id}', [OrderController::class, 'show']);
 Route::put('orders/{id}', [OrderController::class, 'update']);
+Route::get('admin/orders', [OrderManagementController::class, 'index']);
 Route::delete('admin/orders/{id}', [OrderManagementController::class, 'destroy']);
 Route::delete('orders/{id}', [OrderController::class, 'destroy']);
 Route::get('/orders/{order}/status', [OrderController::class, 'status']);
